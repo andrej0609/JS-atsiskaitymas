092cf95be6ae42254235f390abca3f7650705089
andrej0609 <andrejus.galkinas@gmail.com>
2023-01-02 11:53:46 +0200

    4 Užd. padaryta

--- Task 4/script.js
+++ Task 4/script.js
@@ -1,8 +1,8 @@
 /* ------------------------------ TASK 4 -----------------------------------
 Parašykite JS kodą, vartotojui atėjus į tinklapį kreipsis į cars.json failą
 ir iš jo atvaizduos visus automobilių gamintojus ir pagamintus modelius. 
-Kiekvienas gamintojas turės savo atvaizdavimo "kortelę", kurioje bus 
-nurodomas gamintojas ir jo pagaminti modeliai.
+Kiekvienas gamintojas  turės savo atvaizdavimo "kortelę", kurioje bus 
+nurodomas gamintojas + h1(Gamintojas) ir jo pagaminti modeliai + (h2 Modeliai).
 
 
 Pastaba: Informacija apie automobilį (brand) (jo kortelė) bei turi turėti 
@@ -10,3 +10,41 @@ bent minimalų stilių;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'cars.json';
+
+class Car {
+  constructor(brand, models) {
+    this.brand = brand;
+    this.models = models;
+  }
+}
+
+const cars = [];
+
+fetch(ENDPOINT)
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((car) => {
+      cars.push(new Car(car.brand, car.models));
+    });
+    console.log(cars);
+    renderCars(cars);
+  });
+
+function renderCars(cars) {
+  const output = document.querySelector("#output");
+  cars.forEach((car) => {
+    const carDiv = document.createElement("div");
+    carDiv.classList.add("car");
+    const carBrand = document.createElement("h2");
+    carBrand.textContent = car.brand;
+    carDiv.append(carBrand);
+    const carModels = document.createElement("ul");
+    car.models.forEach((model) => {
+      const carModel = document.createElement("li");
+      carModel.textContent = model;
+      carModels.append(carModel);
+    });
+    carDiv.append(carModels);
+    output.append(carDiv);
+  });
+}
