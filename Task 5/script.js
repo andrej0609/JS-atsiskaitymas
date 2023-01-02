16c0ca09dbd7e7323ba47959cf638537cf8af7b2
andrej0609 <andrejus.galkinas@gmail.com>
2023-01-02 12:48:05 +0200

    5 Užd. padaryta

--- Task 5/script.js
+++ Task 5/script.js
@@ -19,3 +19,15 @@ const users = [
   { id: '8', name: 'Simon Peterson', age: 30, hasDog: false },
   { id: '9', name: 'Daniel Cane', age: 51, hasDog: true },
 ];
+
+let filterDogOwners = (users) => {
+  return users.filter((user) => user.hasDog);
+}
+
+let filterAdults = (users) => {
+  return users.filter((user) => user.age >= 18);
+}
+
+console.log(filterDogOwners(users));
+console.log(filterAdults(users));
+
